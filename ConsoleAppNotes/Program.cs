d85ca950e34ff74c73f893394e4aed4f53f3cd6b
Sharikekkk <skvorcovi662@gmail.com>
2022-06-07 13:20:19 +0400

    Добавьте файлы проекта.

---
+++ ConsoleAppNotes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
